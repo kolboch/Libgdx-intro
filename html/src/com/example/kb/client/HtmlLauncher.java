2f2eeec387c4606074c0d5bd9a53b611d48f3c47
Karol <kolboch@gmail.com>
2017-04-19 19:59:34 +0200

    Project setup for CantorGasket drawing
    init setup create with libGdx project creator

---
+++ html/src/com/example/kb/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.example.kb.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.example.kb.CantorGasketDrawing;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new CantorGasketDrawing();
+        }
+}
---
+++ ios/src/com/example/kb/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.example.kb;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.example.kb.CantorGasketDrawing;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new CantorGasketDrawing(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
